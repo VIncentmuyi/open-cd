b00b14783e912d91e14ec2c91d4ae959f2b1093b
VY <vyshooter@gmail.com>
2025-02-21 17:43:41 +0800

    新数据集配置文件

--- command
+++ command
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1 python tools/train.py configs/lightcdnet/lightcdnet_s_256x256_40k_DSIFN.py --work-dir ./Lightcdnet_DSIFN_workdir
-CUDA_VISIBLE_DEVICES=1  python tools/test.py configs/lightcdnet/lightcdnet_s_256x256_40k_DSIFN.py  ./Lightcdnet_DSIFN_workdir/best_mIoU_iter_40000.pth --show-dir tmp_infer
+CUDA_VISIBLE_DEVICES=1 python tools/train.py configs/lightcdnet/lightcdnet_l_512x512_40k_Mine.py --work-dir ./Lightcdnet_Mine_workdir
+CUDA_VISIBLE_DEVICES=1  python tools/test.py configs/lightcdnet/lightcdnet_l_512x512_40k_Mine.py   ./Lightcdnet_DSIFN_workdir/best_mIoU_iter_40000.pth --show-dir tmp_infer
 tmux new -s opencd
 tmux attach -t opencd
---
+++ configs/common/standard_512x512_40k_Mine.py
@@ -0,0 +1,117 @@
+_base_ = '../_base_/default_runtime.py'
+
+dataset_type = 'Mine_Dataset'
+data_root = 'data/Mine'
+
+crop_size = (512, 512)
+train_pipeline = [
+    dict(type='MultiImgLoadImageFromFile'),
+    dict(type='MultiImgLoadAnnotations'),
+    dict(type='MultiImgRandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='MultiImgRandomFlip', prob=0.5),
+    dict(type='MultiImgPackSegInputs')
+]
+test_pipeline = [
+    dict(type='MultiImgLoadImageFromFile'),
+    dict(type='MultiImgResize', scale=(1024, 1024), keep_ratio=True),
+    # add loading annotation after ``Resize`` because ground truth
+    # does not need to do resize data transform
+    dict(type='MultiImgLoadAnnotations'),
+    dict(type='MultiImgPackSegInputs')
+]
+img_ratios = [0.75, 1.0, 1.25]
+tta_pipeline = [
+    dict(type='MultiImgLoadImageFromFile', backend_args=None),
+    dict(
+        type='TestTimeAug',
+        transforms=[
+            [
+                dict(type='MultiImgResize', scale_factor=r, keep_ratio=True)
+                for r in img_ratios
+            ],
+            [
+                dict(type='MultiImgRandomFlip', prob=0., direction='horizontal'),
+                dict(type='MultiImgRandomFlip', prob=1., direction='horizontal')
+            ],
+            [dict(type='MultiImgLoadAnnotations')],
+            [dict(type='MultiImgPackSegInputs')]
+        ])
+]
+train_dataloader = dict(
+    batch_size=8,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='InfiniteSampler', shuffle=True),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        seg_map_suffix='.png',
+        data_prefix=dict(
+            img_path_from='train/A',
+            img_path_to='train/B',
+            seg_map_path='train/label'),
+        pipeline=train_pipeline))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        seg_map_suffix='.png',
+        data_prefix=dict(
+            img_path_from='val/A',
+            img_path_to='val/B',
+            seg_map_path='val/label'),
+        pipeline=test_pipeline))
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        seg_map_suffix='.png',
+        data_prefix=dict(
+            img_path_from='test/A',
+            img_path_to='test/B',
+            seg_map_path='test/label'),
+        pipeline=test_pipeline))
+
+val_evaluator = dict(type='mmseg.IoUMetric', iou_metrics=['mFscore', 'mIoU'])
+test_evaluator = dict(
+    type='mmseg.IoUMetric',
+    iou_metrics=['mFscore', 'mIoU'])
+
+# optimizer
+optimizer=dict(
+    type='AdamW', lr=0.001, betas=(0.9, 0.999), weight_decay=0.05)
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer)
+# learning policy
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=1e-6, by_epoch=False, begin=0, end=1000),
+    dict(
+        type='PolyLR',
+        power=1.0,
+        begin=1000,
+        end=40000,
+        eta_min=0.0,
+        by_epoch=False,
+    )
+]
+# training schedule for 40k
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=40000, val_interval=4000)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=4000,
+                    save_best='mIoU'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='CDVisualizationHook', interval=1,
+                       img_shape=(1024, 1024, 3)))
